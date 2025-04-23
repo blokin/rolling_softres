b290afb7088c785ecd79d7534a180d9791e48a6f
blokin <46669651+blokin@users.noreply.github.com>
2025-04-23 06:33:43 -0400

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<HTML>
-  <BODY>
-    <form action="softres.php" method="POST">
-      <textarea name="CSV" cols="40" rows="5">Paste CSV data here</textarea>
-      <input type="submit">
-    </form>
-  </BODY>
-</HTML>
